26f3fa71f0943ba7c0f0f5e67db283c32c4a5430
enzosdev <enzohfs@gmail.com>
2025-10-27 20:58:49 -0300

    feat: Adding EnableFeignClients and EnableCaching to CartServiceApplication

--- src/main/java/enzosdev/cartservice/CartserviceApplication.java
+++ src/main/java/enzosdev/cartservice/CartserviceApplication.java
@@ -2,8 +2,12 @@ package enzosdev.cartservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
+@EnableCaching
 public class CartserviceApplication {
 
     public static void main(String[] args) {
