5326b3590f4de0f8d1280cec7c3b5864946fffc4
enzosdev <enzohfs@gmail.com>
2025-10-27 21:01:20 -0300

    feat: DummyJsonClient interface that have getAllProducts and getProductById endpoints to request the API

--- src/main/java/enzosdev/cartservice/client/DummyJsonClient.java
+++ src/main/java/enzosdev/cartservice/client/DummyJsonClient.java
@@ -1,4 +1,17 @@
-package enzosdev.cartservice;
+package enzosdev.cartservice.client;
 
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "dummy", url = "${cart.client.dummy}")
 public interface DummyJsonClient {
+    @GetMapping("/products")
+      ProductListResponse getAllProducts();
+
+    @GetMapping("/products/{id}")
+    DummyProductResponse getProductById(@PathVariable Long id);
 }
