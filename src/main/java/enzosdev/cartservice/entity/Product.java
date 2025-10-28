adce0fbaf27c8df305cefc5282113ea2ca8c2753
enzosdev <enzohfs@gmail.com>
2025-10-27 21:18:33 -0300

    feat: Product Entity

--- src/main/java/enzosdev/cartservice/entity/Product.java
+++ src/main/java/enzosdev/cartservice/entity/Product.java
@@ -1,4 +1,17 @@
 package enzosdev.cartservice.entity;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Builder
+@Getter
 public class Product {
+    private Long id;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
 }
