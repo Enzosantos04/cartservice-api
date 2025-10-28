e76b34d6591c3b826d01c1449104d3f617889cd6
enzosdev <enzohfs@gmail.com>
2025-10-27 21:25:56 -0300

    feat: CartRepository extending mongoRepository to create the cart entity having derived queries findByClientAndStatus

--- src/main/java/enzosdev/cartservice/repository/CartRepository.java
+++ src/main/java/enzosdev/cartservice/repository/CartRepository.java
@@ -1,10 +1,11 @@
-package enzosdev.cartservice;
+package enzosdev.cartservice.repository;
 
-import jdk.jshell.Snippet;
+import enzosdev.cartservice.entity.Cart;
+import enzosdev.cartservice.enums.Status;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface repository extends MongoRepository<Cart, String> {
-    Optional<Cart> findByClientAndStatus(Long client, )
+public interface CartRepository extends MongoRepository<Cart, String> {
+    Optional<Cart> findByClientAndStatus(Long client, Status status);
 }
