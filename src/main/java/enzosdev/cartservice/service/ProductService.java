02e6781425a50405e2909c6a7834ceb16a5ee13a
enzosdev <enzohfs@gmail.com>
2025-10-27 21:28:09 -0300

    feat: business rule for product service

--- src/main/java/enzosdev/cartservice/service/ProductService.java
+++ src/main/java/enzosdev/cartservice/service/ProductService.java
@@ -1,4 +1,33 @@
 package enzosdev.cartservice.service;
 
+
+import enzosdev.cartservice.client.DummyJsonClient;
+import enzosdev.cartservice.client.DummyProductResponse;
+import enzosdev.cartservice.client.ProductListResponse;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    private final DummyJsonClient dummyJsonClient;
+
+    public ProductService(DummyJsonClient dummyJsonClient){
+        this.dummyJsonClient = dummyJsonClient;
+    }
+
+
+    @Cacheable(value = "products")
+    public ProductListResponse getAllProducts(){
+        return dummyJsonClient.getAllProducts();
+    }
+
+    @Cacheable(value = "products", key = "#productId")
+        public DummyProductResponse getAllProductById(Long productId){
+        return dummyJsonClient.getProductById(productId);
+    }
+
+
 }
