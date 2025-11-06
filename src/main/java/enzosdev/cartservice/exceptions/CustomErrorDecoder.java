a6ac087d4c0f3e891770184f9b7460846087f219
enzosdev <enzohfs@gmail.com>
2025-11-06 19:11:52 -0300

    feat: CustomErrorDecoder is personlized exception using if statement

--- src/main/java/enzosdev/cartservice/exceptions/CustomErrorDecoder.java
+++ src/main/java/enzosdev/cartservice/exceptions/CustomErrorDecoder.java
@@ -1,4 +1,14 @@
 package enzosdev.cartservice.exceptions;
 
-public class CustomErrorDecoder {
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+public class CustomErrorDecoder implements ErrorDecoder {
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        if (response.status() == 400) {
+            return new DataNotFoundException("Product not Found");
+        }
+        return new Exception("Exception while getting product");
+    }
 }
